be7dd475b0658988e8622babe93ac1e9688907b0
AtelierDiamond <ateliershivendra@gmail.com>
2025-01-16 17:51:16 +0530

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+         <h1 className="font-lg text-lg">Welcome to Jewellery B2B Platform</h1>
+         <span>Powered by Diamond Atelier</span>
+    </div>
+  );
+}
