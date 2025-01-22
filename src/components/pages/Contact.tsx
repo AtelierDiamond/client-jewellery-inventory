09086339b48b416755e9839da57b632931153a89
muskanDaruka <darukamuskan@gmail.com>
2025-01-22 15:33:54 -0800

    fix: css

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -11,7 +11,7 @@ function Contact() {
                     <div className='w-auto h-10 text-white bg-[#B7A99A] px-8 py-12 text-2xl rounded-xl flex items-center justify-center'>Ritu Chugh +1 (917) 385-2648</div>
                 </div>
                 <h2 className='text-2xl font-normal text-black my-6'>Send your message</h2>
-                <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-2 xl:gap-8'>
                     <div>
                         <input
                             type="text"
